a175b93711c98cc75d1e1c638c7fe5ccd84b4eed
2FLing <349332929yaofu@gmail.com>
2019-09-02 10:10:33 -0700

    upload ADT

--- Abstract Data Type.cpp
+++ Abstract Data Type.cpp
@@ -1,22 +1,39 @@
 #include<stdio.h>
 #include<random>
-#include"TemplateBag.h"
-#include"Rectangle.h"
+#include"bag.h"
 using namespace std;
 int main()
 {
-	Bag<char> myBag;
-	Bag<int>myBag_2;
-	Bag<Rectangle>RecBag;
-	Rectangle rc1(2,3);
-	int num;
-	myBag.add('u');
-	myBag.add('&');
-	myBag.add('w');
-	myBag.display();
-	myBag_2.add(2);
-	myBag_2.display();
-	RecBag.add(rc1);
-	RecBag.display();
+	Bag myBag;
+	int attemp = 0,guess_num,score=0;
+	for (int i = 0; i < 6; i++)
+		myBag.add(rand() % 100);//Add 6 random numbers in range 0-99 to the bag
+	while (attemp <10 and !myBag.isEmpty())//if attemp is less then 10 and bag is not empty keep asking the user
+	{
+		attemp++;
+		cout << "Can you guess what number is in the bag?" << endl;
+		cin >> guess_num;
+		if (myBag.contains(guess_num))//if the guess number is in the bag
+		{
+			cout << "Right!" << endl;
+			score += 10;
+			myBag.remove(guess_num);
+		}
+		else
+		{
+			cout << "This number is not inside of the bag!" << endl;
+		}
+	}
+	if (attemp < 10)//If the times of attemp is less then 10 and bag is empty
+	{
+		score += 200;//Add extra 200 points
+		cout << "Game over!" << endl;
+	}
+	if(attemp==10 and !myBag.isEmpty())//If the user reached 10 attemps and the bag is not empty
+	{
+		cout << "Game over!" << endl;
+		cout << "Your socre is : " << score << endl;//Display the score
+	}
+
 	return 0;
 }
